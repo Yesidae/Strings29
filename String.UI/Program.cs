f79906e4021515d206f9ee1d0d828fc81d554ba1
Yesidae <yesidae.1070@gmail.com>
2023-03-16 22:10:52 -0500

    Add project files.

---
+++ String.UI/Program.cs
@@ -0,0 +1,13 @@
+﻿using String.Logic;
+
+var myStrings = new Strings29[]
+{
+    new Strings29("*==+===+==*"),
+    new Strings29("*=+="),
+    new Strings29("*====="),
+};
+
+foreach (var myString in myStrings)
+{
+    Console.WriteLine(myString.Bridge());
+}
---
+++ String29.Logic/Strings29.cs
@@ -0,0 +1,46 @@
+﻿namespace String.Logic
+{
+    public class Strings29
+    {
+        private readonly string _bridge;
+
+        public Strings29(string text)
+        {
+            _bridge = text;
+        }
+
+        public bool Bridge()
+        {
+            string bridge = _bridge;
+            int booster = 0; //refuerzo
+            int platform = 0; //plataforma
+
+            int n = bridge.Length / 2;
+            if (bridge[0] != '*')
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (bridge[i] != bridge[bridge.Length - i - 1])
+                    {
+                        return false;
+                    }
+
+                    //if (bridge[i] == '=' || bridge[i] == '+')
+                    //{
+                    //    if (bridge[i] == '=')
+                    //    {
+                    //        platform++;
+                    //    }
+
+                    //    if (platform == 3)
+                    //    {
+                    //        return false;
+                    //    } 
+                    //}
+                }
+                
+            }
+            return true;
+        }
+    }
+}
